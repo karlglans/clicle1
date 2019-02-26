1f343c8192a3f4db8593644811fe30f000040646
karlglans <karl_glans@hotmail.com>
2019-02-26 18:41:24 +0100

    fixing code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-          </b>
             Learn React
           </a>
         </header>
